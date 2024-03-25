0ea38bde8c674e5eeab9c2df8f920b30d4cb7fcf
Hubert Dumas <hubert.dumas@zincgroup.com>
2024-03-25 16:46:39 +1100

    react client

---
+++ react-amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "reactamplifyd53e8d38": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ zinc-fed/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+   https: {
+      key: './localhost-key.pem',
+      cert: './localhost.pem'
+   },
+   port: 4310
+  }
+})
