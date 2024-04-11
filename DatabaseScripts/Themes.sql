b03fb6deb9a0abbc671afb90b533cecd9f38216b
Hubert Dumas <huberd@gmail.com>
2024-04-12 09:07:10 +1000

    SQL changes

--- DatabaseScripts/Themes.sql
+++ DatabaseScripts/Themes.sql
@@ -1,5 +1,7 @@
-INSERT Store_Settings(StoreID, SettingName, SettingValue, DigitalOnly)
-SELECT 55, 'ColorScheme', 'dark', 0
+-- NO LONGER REQUIRED - MANTINE UI CAN SUCK *****
 
-INSERT Store_Settings(StoreID, SettingName, SettingValue, DigitalOnly)
-SELECT 55, 'Theme', '{"fontFamily":"''Ruda'', sans-serif","headings":{"sizes":{"h1":{"fontSize":"32","fontWeight":"500"},"h2":{"fontSize":"24","fontWeight":"500"},"h3":{"fontSize":"20","fontWeight":"500"},"h4":{"fontSize":"16","fontWeight":"500"}}}}', 0
+-- INSERT Store_Settings(StoreID, SettingName, SettingValue, DigitalOnly)
+-- SELECT 55, 'ColorScheme', 'dark', 0
+
+-- INSERT Store_Settings(StoreID, SettingName, SettingValue, DigitalOnly)
+-- SELECT 55, 'Theme', '{"fontFamily":"''Ruda'', sans-serif","headings":{"sizes":{"h1":{"fontSize":"32","fontWeight":"500"},"h2":{"fontSize":"24","fontWeight":"500"},"h3":{"fontSize":"20","fontWeight":"500"},"h4":{"fontSize":"16","fontWeight":"500"}}}}', 0
